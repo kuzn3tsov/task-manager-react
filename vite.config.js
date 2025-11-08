c0feba75a893b114e45dc7f17fba3b4059b6f1d8
kuzn3tsov <lem.treursic@gmail.com>
2025-11-08 18:47:13 +0100

    Deploy to GH Pages

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/task-manager-react/', // Change this line
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    emptyOutDir: true
-  },
-  server: {
-    port: 3000
-  }
+  base: '/task-manager-react/', // Make sure this matches your repo name exactly
 })
